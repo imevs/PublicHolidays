8ba10bcae35b159b1ce7eac02388db9e63f9834e
Vitaliy Medvedev <medvedev.vs@gmail.com>
2025-08-31 15:23:11 +0000

    refactor: simplify holiday filtering in useCalendar tests

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   ignorePatterns: ['scripts', 'dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
