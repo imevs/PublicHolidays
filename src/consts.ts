d75ea4e1bddad209b7ed033e2d18f01e010397dc
Vitaly Medvedev <medvedev.vs@gmail.com>
2025-12-25 02:20:35 +0200

    pass env variable - option 3

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,4 @@
-export const APP_BASE_NAME =
-    typeof process !== "undefined"
-        ? process.env.APP_BASE_NAME ?? "PublicHolidays/main"
-        : "PublicHolidays/main";
+export const APP_BASE_NAME = process.env.APP_BASE_NAME ?? "PublicHolidays/main";
 
 export const STORAGE_KEY = "holidaysData";
 export const dateLocale = "en-CA"; // Canadian English uses "YYYY-MM-DD, HH:mm" format
